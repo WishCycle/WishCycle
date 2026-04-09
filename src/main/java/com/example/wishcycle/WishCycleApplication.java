e896781bda2af26370ef7444695d7d0d2b951464
Mowgli-dk <emiland4@gmail.com>
2026-04-09 16:02:25 +0200

    Initial Commit

---
+++ src/main/java/com/example/wishcycle/WishCycleApplication.java
@@ -0,0 +1,13 @@
+package com.example.wishcycle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishCycleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WishCycleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wishcycle/WishCycleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wishcycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishCycleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
