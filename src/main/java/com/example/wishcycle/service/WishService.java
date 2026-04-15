2d2475a62359a8b15f521ecd8b0a6ee65b7632cd
Simon BC <simon.bech.christensen.00@gmail.com>
2026-04-15 08:48:31 +0200

    created skeleton of service class

--- src/main/java/com/example/wishcycle/service/WishService.java
+++ src/main/java/com/example/wishcycle/service/WishService.java
@@ -1,9 +1,55 @@
 package com.example.wishcycle.service;
 
+import com.example.wishcycle.model.Item;
+import com.example.wishcycle.model.WishList;
+import com.example.wishcycle.repository.jdbc.MemberRepository;
+import com.example.wishcycle.repository.jdbc.WishListRepository;
+import com.example.wishcycle.repository.jdbc.WishRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class WishService {
+    private final MemberRepository memberRepository;
+    private final WishListRepository wishListRepository;
+    private final WishRepository wishRepository;
+
+
+    public WishService(MemberRepository memberRepository, WishListRepository wishListRepository, WishRepository wishRepository) {
+        this.memberRepository = memberRepository;
+        this.wishListRepository = wishListRepository;
+        this.wishRepository = wishRepository;
+    }
+
+    public List<WishList> getPublicWishLists() {
+
+    }
+
+    public List<WishList> getPrivateWishLists() {
+
+    }
+
+    public void createWishList(WishList wishList) {
+
+    }
+
+    public void updateWishList(WishList wishList, List<Item> list) {
+
+    }
+
+    public void deleteWishList(WishList wishList) {
+
+    }
+
+    public void createWish(Item wish) {
+
+    }
+
+    public void deleteWish(Item wish) {
+
+    }
+
 
 
 
