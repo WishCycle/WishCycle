cfbd218290feba27ba3f313ca24367c72fcc9c0c
Joakim700 <Joakim.b.eskildsen@gmail.com>
2026-04-21 11:44:35 +0200

    Memberservice test

--- src/main/java/com/example/wishcycle/repository/jdbc/MemberRepository.java
+++ src/main/java/com/example/wishcycle/repository/jdbc/MemberRepository.java
@@ -10,14 +10,12 @@ public class MemberRepository {
 
     private final JdbcTemplate jdbc;
     private final MemberMapper memberMapper;
-
     private static final String FIND_BY_USERNAME_SQL = "SELECT * FROM wish_user WHERE username = ?";
     private static final String FIND_BY_EMAIL_SQL = "SELECT * FROM wish_user WHERE user_email = ?";
     private static final String FIND_BY_ID_SQL = "SELECT * FROM wish_user WHERE user_id = ?";
     private static final String DELETE_MEMBER_SQL = "DELETE FROM wish_user WHERE user_id = ?";
     private static final String CREATE_MEMBER_SQL = "INSERT INTO wish_user (username, user_email, user_password) VALUES (?, ?, ?)";
     private static final String UPDATE_MEMBER_SQL = "UPDATE wish_user SET username = ?, user_email = ?, user_password = ? WHERE user_id = ?";
-
 
     public MemberRepository(JdbcTemplate jdbc) {
         this.jdbc = jdbc;
