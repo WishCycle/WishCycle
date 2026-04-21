f036cda0c96270fe84e1ea4990dce3398cd03293
Mowgli-dk <emiland4@gmail.com>
2026-04-21 19:14:22 +0200

    fixing login paths

--- src/main/java/com/example/wishcycle/config/WebConfig.java
+++ src/main/java/com/example/wishcycle/config/WebConfig.java
@@ -15,7 +15,25 @@ public class WebConfig  implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(loginInterceptor)
-                .addPathPatterns("/**/wishlists/**", "/**/login/profile/**", "/**/wishlist/**", "/**/item/**") // Tell Spring which URLs to watch
-                .excludePathPatterns("/**/homepage", "/**/login-page", "/**/signup-page", "/**/css/**", "/**/images/**");   // And these to be excluded
+                .addPathPatterns( // Tell Spring which URLs to watch
+                        "/**/homepage",
+                        "/**/wishlists/**",
+                        "/**/wishlist/**",
+                        "/**/social-wishlists/**", // Added to protect friend lists
+                        "/**/login/profile/**",
+                        "/**/item/**"
+                ) // Tell Spring which URLs to watch
+
+                .excludePathPatterns( // And these to be excluded
+                        "/**/index",           // The landing page
+                        "/**/login-page",      // Need to see this to log in!
+                        "/**/signup-page",     // Need to see this to join!
+                        "/**/login/save",      // The POST route for logging in
+                        "/**/signup/save",     // The POST route for signing up
+                        "/**/about-us",        // Usually public
+                        "/**/css/**",          // Static files must be public
+                        "/**/images/**",
+                        "/**/js/**"
+                );
     }
 }
